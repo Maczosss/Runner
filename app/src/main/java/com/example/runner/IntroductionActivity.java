ccec871ce960b93901563223dbc47b62bcaf55d7
Maciej Jaremowicz <maciej.jaremowicz@gmail.com>
2021-11-28 19:34:03 +0100

    before_destruction_by_my_own_code

--- app/src/main/java/com/example/runner/IntroductionActivity.java
+++ app/src/main/java/com/example/runner/IntroductionActivity.java
@@ -2,18 +2,23 @@ package com.example.runner;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.view.animation.Animation;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.airbnb.lottie.LottieAnimationView;
 
-import fragments.WeekPlanListFragment;
+import fragments.FirstFragment;
+import fragments.SecondFragment;
+import fragments.ThirdFragment;
 import listeners.ProgressSaver;
 import plan.WeeksPlan;
 
@@ -21,9 +26,20 @@ public class IntroductionActivity extends AppCompatActivity {
 
     ImageView background, name, name2;
     LottieAnimationView lottie;
-    private Button startBtn, chooseBtn;
+//    private Button startBtn, chooseBtn;
     TextView infoAboutCurrentWorkout;
     ProgressSaver saver = ProgressSaver.getInstance();
+
+    //sliding fragments
+    private static final int NUMBER_OF_FRAGMENTS = 3;
+    private ViewPager pager;
+    private ScreenSlideAdapter pagerAdapter;
+
+
+    //animation
+
+    Animation animation;
+
     //TODO list for choosing workplan on application start
     WeeksPlan[] plan = WeeksPlan.values();
     ListView listOfPlans;
@@ -41,9 +57,17 @@ public class IntroductionActivity extends AppCompatActivity {
         background = findViewById(R.id.backgroundIntroduction);
         name = findViewById(R.id.nameLogo);
         name2 = findViewById(R.id.nameLogo2);
-        startBtn = findViewById(R.id.startBtn);
-        chooseBtn = findViewById(R.id.chooseBtn);
-        infoAboutCurrentWorkout = findViewById(R.id.textFieldIntroductionInformation);
+//        startBtn = findViewById(R.id.startBtn);
+//        chooseBtn = findViewById(R.id.chooseBtn);
+//        infoAboutCurrentWorkout = findViewById(R.id.textFieldIntroductionInformation);
+
+        //liquid pager for swiping
+        pager = findViewById(R.id.liquid_pager);
+        pagerAdapter = new ScreenSlideAdapter(getSupportFragmentManager());
+        pager.setAdapter(pagerAdapter);
+
+        //todo animation is next
+        //animation = AnimationUtils.loadAnimation(this, R.anim.animation);
 
         //=============================
 
@@ -51,53 +75,53 @@ public class IntroductionActivity extends AppCompatActivity {
 
 
         //============================
-        listOfPlans = findViewById(R.id.list);
+//        listOfPlans = findViewById(R.id.list);
 
-        String[] test = {
-                "Maciej",
-                "Sraciej",
-                "Kutaciej",
-        };
-
-        ArrayAdapter<String> arr;
-        arr
-                = new ArrayAdapter<String>(
-                this,
-                R.layout.support_simple_spinner_dropdown_item,
-                test);
-        listOfPlans.setAdapter(arr);
+//        String[] test = {
+//                "Maciej",
+//                "Sraciej",
+//                "Kutaciej",
+//        };
+//
+//        ArrayAdapter<String> arr;
+//        arr
+//                = new ArrayAdapter<String>(
+//                this,
+//                R.layout.support_simple_spinner_dropdown_item,
+//                test);
+//        listOfPlans.setAdapter(arr);
 
         //=====================
 
-        if(saver.getCurrentDayOfWeek()!=1 && saver.getWeek()!=1) {
-            infoAboutCurrentWorkout.setText(String.format("You are on week: %d, and day: %d",
-                    ProgressSaver.getInstance().getWeek(),
-                    ProgressSaver.getInstance().getCurrentDayOfWeek()));
-        }else{
-            infoAboutCurrentWorkout.setText("Lets go running, start from beginning \n" +
-                    " or choose starting day");
-        }
+//        if(saver.getCurrentDayOfWeek()!=1 && saver.getWeek()!=1) {
+//            infoAboutCurrentWorkout.setText(String.format("You are on week: %d, and day: %d",
+//                    ProgressSaver.getInstance().getWeek(),
+//                    ProgressSaver.getInstance().getCurrentDayOfWeek()));
+//        }else{
+//            infoAboutCurrentWorkout.setText("Lets go running, start from beginning \n" +
+//                    " or choose starting day");
+//        }
 
 
-        startBtn.setOnClickListener(v -> {
-            start();
-        });
+//        startBtn.setOnClickListener(v -> {
+//            start();
+//        });
 
-        chooseBtn.setOnClickListener(v->{
-//            listOfPlans.setVisibility(View.VISIBLE);
-//            FragmentManager fm = getSupportFragmentManager();
-//            fragment = fm.findFragmentByTag("myFragmentTag");
-//            if (fragment == null) {
-//                FragmentTransaction ft = fm.beginTransaction();
-//                fragment =new WeekPlanListFragment();
-//                ft.add(android.R.id.content,fragment,"myFragmentTag");
-//                ft.commit();
-//            }
-
-            Intent intent;
-            intent = new Intent(getApplicationContext(), WeekPlanListFragment.class);
-            startActivity(intent);
-        });
+//        chooseBtn.setOnClickListener(v->{
+////            listOfPlans.setVisibility(View.VISIBLE);
+////            FragmentManager fm = getSupportFragmentManager();
+////            fragment = fm.findFragmentByTag("myFragmentTag");
+////            if (fragment == null) {
+////                FragmentTransaction ft = fm.beginTransaction();
+////                fragment =new WeekPlanListFragment();
+////                ft.add(android.R.id.content,fragment,"myFragmentTag");
+////                ft.commit();
+////            }
+//
+//            Intent intent;
+//            intent = new Intent(getApplicationContext(), WeekPlanListFragment.class);
+//            startActivity(intent);
+//        });
 
         lottie = findViewById(R.id.lottie);
 
@@ -108,15 +132,47 @@ public class IntroductionActivity extends AppCompatActivity {
 
         lottie.animate().translationY(1600).setDuration(1000).setStartDelay(1000).alpha(0);
 
-        startBtn.animate().alpha(1).setDuration(1000).setStartDelay(1400);
-        name2.animate().alpha(1).setDuration(1500).setStartDelay(1600);
-        chooseBtn.animate().alpha(1).setDuration(1000).setStartDelay(1400);
-        infoAboutCurrentWorkout.animate().translationY(-100).alpha(1).setDuration(1500).setStartDelay(1400);
+//        startBtn.animate().alpha(1).setDuration(1000).setStartDelay(1400);
+//        name2.animate().alpha(1).setDuration(1500).setStartDelay(1600);
+//        chooseBtn.animate().alpha(1).setDuration(1000).setStartDelay(1400);
+//        infoAboutCurrentWorkout.animate().translationY(-100).alpha(1).setDuration(1500).setStartDelay(1400);
     }
 
     public void start() {
         Intent intent;
         intent = new Intent(getApplicationContext(), TimerActivity.class);
         startActivity(intent);
+    }
+
+    //todo get rid of depricated interface
+    private class ScreenSlideAdapter extends FragmentStatePagerAdapter {
+
+
+        public ScreenSlideAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    FirstFragment tab1 = new FirstFragment();
+                    return tab1;
+                case 1:
+                    SecondFragment tab2 = new SecondFragment();
+                    return tab2;
+                case 2:
+                    ThirdFragment tab3 = new ThirdFragment();
+                    return tab3;
+                default:
+                    return new Fragment();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return NUMBER_OF_FRAGMENTS;
+        }
     }
 }
